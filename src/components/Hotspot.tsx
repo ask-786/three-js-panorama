758863e1b07c292d17c2280f381ddaeb17a3c0e0
Ahmad bin Marakkar <ahmadshabeerk786@gmail.com>
2025-03-15 14:47:51 +0530

    chore: more experiments

--- src/components/Hotspot.tsx
+++ src/components/Hotspot.tsx
@@ -1,14 +1,45 @@
-const Hotspot = ({
-  position,
-  onClick,
-}: {
-  position: [number, number, number];
-  onClick: () => void;
-}) => (
-  <mesh position={position} onClick={onClick}>
-    <sphereGeometry args={[5, 16, 16]} />
-    <meshBasicMaterial color="red" />
-  </mesh>
-);
+import { useEffect, useState } from "react";
+
+export const HotspotState = {
+  ENTERED: "ENTERED",
+  LEFT: "LEFT",
+};
+
+export type HotspotState = (typeof HotspotState)[keyof typeof HotspotState];
+
+type Props = { position: [number, number, number]; onClick: () => void };
+
+const Hotspot = ({ position, onClick }: Props) => {
+  const [state, setState] = useState<HotspotState>(HotspotState.LEFT);
+
+  useEffect(() => {
+    document.body.style.cursor =
+      state === HotspotState.ENTERED ? "pointer" : "auto";
+  }, [state]);
+
+  const onPointerEnter = () => {
+    setState(HotspotState.ENTERED);
+  };
+
+  const onPointerLeave = () => {
+    setState(HotspotState.LEFT);
+  };
+
+  return (
+    <mesh
+      onPointerEnter={onPointerEnter}
+      onPointerLeave={onPointerLeave}
+      position={position}
+      onClick={onClick}
+    >
+      <sphereGeometry
+        args={[HotspotState.ENTERED === state ? 10 : 5, 16, 16]}
+      />
+      <meshBasicMaterial
+        color={state === HotspotState.ENTERED ? "blue" : "red"}
+      />
+    </mesh>
+  );
+};
 
 export default Hotspot;
