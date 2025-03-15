c38693720665d12235edaf8b123529e11c913b3f
Ahmad bin Marakkar <ahmadshabeerk786@gmail.com>
2025-03-15 13:25:39 +0530

    more update

--- src/components/Panorama.tsx
+++ src/components/Panorama.tsx
@@ -79,7 +79,7 @@ const PanoramaView = ({ textures }: { textures: string[] }) => {
       <Hotspot position={[-300, 50, -200]} onClick={() => changeTexture(2)} />
       <Hotspot position={[100, -100, 60]} onClick={() => changeTexture(0)} />
 
-      <OrbitControls enableZoom={true} enablePan={true} rotateSpeed={-0.5} />
+      <OrbitControls enableZoom={true} enablePan={false} rotateSpeed={-0.5} />
     </>
   );
 };
@@ -92,7 +92,10 @@ const Panorama = () => {
   ];
 
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
+    <div className="relative" style={{ width: "100vw", height: "100vh" }}>
+      <span className="absolute z-10 top-5 left-5 text-3xl font-semibold">
+        Panorama Demo
+      </span>
       <Canvas camera={{ position: new Vector3(0, 0, 0.1) }}>
         <pointLight position={[10, 10, 10]} />
         <PanoramaView textures={textures} />
